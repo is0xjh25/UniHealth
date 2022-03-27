b83f81dcd9483661415038fb6894e798b2232925
Yun-Chi Hsiao <is0.jimhsiao@gmail.com>
2022-03-27 16:29:07 +0800

    Update: Restructure directories

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const res = require('express/lib/response')
+const app = express()
+const port = 3000
+var path = require('path')
+app.use(express.static(path.join(__dirname, '/public')));
+
+
+/* Genral */
+app.get('/', (req, res) => {
+	res.send('<h1>Hello World!</h1>')
+})
+ 
+app.get('/about-us', (req, res) => {
+	res.sendFile(__dirname + '/static/' + 'about-us.html')
+})
+
+app.get('/about-diabetes', (req, res) => {
+	res.sendFile(__dirname + '/static/' + 'about-diabetes.html')
+})
+
+/* Clinician */
+
+/* Patient */
+
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`)
+})
