c7360d6975fb43162435225c99e47e8d065328cb
Yun-Chi Hsiao <is0.jimhsiao@gmail.com>
2022-03-27 17:00:36 +0800

    Modify: Rename file

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const res = require('express/lib/response')
 const app = express()
 const port = 3000
 var path = require('path')
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/')));
 
 
 /* Genral */
