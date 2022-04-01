c57bc888e5347fb0deef9b787415f61cd5cad946
Yun-Chi Hsiao <is0.jimhsiao@gmail.com>
2022-04-01 15:12:54 +0800

    Update: demo get example

--- controllers/demoController.js
+++ controllers/demoController.js
@@ -4,6 +4,17 @@ const getAllDemoData = (req, res) => {
     res.send(demoData) // send list to browser
 }
 
+const getDataById = (req, res) => {
+    // search the database by ID
+    const data = demoData.find(data => data.id === req.params.id)
+    if (data) {
+        res.send(data)
+    } else {
+        res.sendStatus(404)
+    } 
+}
+
 module.exports = {
-    getAllDemoData
+    getAllDemoData,
+    getDataById
 }
--- routes/demoRouter.js
+++ routes/demoRouter.js
@@ -2,5 +2,6 @@ const express = require('express')
 const demoRouter = express.Router()
 const demoController = require('../controllers/demoController')
 
-demoRouter.get('/', demoController.getAllDemoData)
-module.exports = demoRouter
+demoRouter.get('/', demoController.getAllDemoData);
+demoRouter.get('/:id', demoController.getDataById);
+module.exports = demoRouter;
