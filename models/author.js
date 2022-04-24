c603ae521a4393760fb1c60f941ee396dbbef0cc
Yun-Chi Hsiao <is0.jimhsiao@gmail.com>
2022-04-24 23:29:18 +0800

    Update: demo

---
+++ models/author.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+    first_name: String,
+    last_name: String
+})
+
+const Author = mongoose.model('Author', schema)
+module.exports = Author
---
+++ models/index.js
@@ -0,0 +1,22 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost' , {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'demo'
+})
+
+const db = mongoose.connection.on('error', err => {
+    console.error(err)
+    process.exit(1)
+})
+
+db.once('open', async () => {
+    console.log(`Mongo connection started on ${db.host}:${db.port}`)
+})
+
+require('./author')
